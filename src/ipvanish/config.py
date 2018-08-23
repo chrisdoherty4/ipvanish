cba3044ef2b8ba30f09b123ae07067dbdc398551
Chris Doherty <chris.doherty4@gmail.com>
2018-08-23 20:52:03 +0100

    Add the config and setup.py objects (setup.py placeholder)

---
+++ src/ipvanish/config.py
@@ -0,0 +1,51 @@
+import os
+"""
+This module contains the application configuration values.
+"""
+
+config = {}
+
+""" General """
+
+"""
+The root configuration directory for the application.
+"""
+config["config.dir"] = os.path.abspath(
+    os.path.expanduser(
+        os.path.join('~', '.config', 'vanish')
+        )
+    )
+
+""" GeoJson """
+
+"""
+URL to the IPVanish geojson. The geojson contains server status information
+and contributes to how we interface witht he ovpn configuration files.
+"""
+config["geojson.url"] = "https://www.ipvanish.com/api/servers.geojson"
+
+
+"""
+A path to the geojson cache location.
+"""
+config['geojson.cache'] = os.path.join(config['config.dir'], 'servers.geojson')
+
+"""
+A timeout value indicating how long the geojson is valid for.
+"""
+config['geojson.cache.timeout'] = 30
+
+"""
+OpenVPN
+"""
+
+"""
+URL to IPVanish ovpn configuration files. This is a zip containing .ovpn files
+and the necessary certificate file to connect to IPVanish servers.
+"""
+config["ovpn.configs.url"] = "https://www.ipvanish.com/software/configs/configs.zip"
+
+"""
+The location the ovpn configuration files will be stored.
+"""
+config["ovpn.configs.dir"] = os.path.join(config['config.dir'], 'openvpn')
